e6aec505df5405d90bbcacb0865071a9d8767e63
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-01-31 12:37:54 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 100; i > 0; i--) {
+		cout << i << endl;
+		cout << " bottles of rootbeer on the wall,  bottles of rootbeer, take one down pass it round" << endl;
+	}
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+}
